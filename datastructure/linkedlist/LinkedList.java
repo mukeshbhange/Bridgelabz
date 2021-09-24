0d49b108c03d6de94e1e384eb65c1c46f352abef
mukeshbhange <bhangemukesh98@gmail.com>
2021-09-24 18:47:42 +0530

    Second push

---
+++ datastructure/linkedlist/LinkedList.java
@@ -0,0 +1,107 @@
+package com.fellowship.datastructure.linkedlist;
+
+class Node{
+	int data;
+	Node nextAddress;
+	
+	public Node(int data) {
+		this.data = data;
+		this.nextAddress = null;
+	}	
+}
+public class LinkedList {
+	static Node firstNode;
+	public void add(int data) {
+		Node newNode = new Node(data);
+
+		if(firstNode == null) {
+			firstNode = newNode;
+		}else {
+			Node temp = firstNode;
+			//newNode.nextAddress = null;
+			while(temp.nextAddress != null) {
+				temp = temp.nextAddress;
+			}
+			temp.nextAddress = newNode;
+		}
+	}
+
+	public void addDataAtFront(int data) {
+		Node newNode = new Node(data);
+		newNode.nextAddress = firstNode;
+		firstNode = newNode;
+	}
+	
+	public void display() {
+		Node temp = firstNode;
+		if(temp == null) {
+			System.out.println("List is Empty.Add data first.");
+		}
+		while(temp!= null) {
+			System.out.print(temp.data+"->");
+			temp = temp.nextAddress; 
+		}
+		System.out.println();
+	}
+	
+	public int sizeOfList() {
+		Node temp = firstNode;
+		int count = 0;
+		while(temp != null) {
+			count += 1;
+			temp = temp.nextAddress; 
+		}
+		return count;	
+	}
+	
+	public boolean search(int data) {
+		Node temp = firstNode;
+		boolean search = false;
+		while(temp!= null ) {
+			if(temp.data == data) {
+				search = true;
+				return search;
+			}else {
+				temp = temp.nextAddress; 
+			}
+		}
+		return search;	
+	}
+	
+	public void deleteFromEnd() {
+		Node temp = firstNode;
+		Node parent = null;
+		while(temp.nextAddress != null) {
+			parent = temp;
+			temp = temp.nextAddress; 
+		}
+		parent.nextAddress = null;
+	}
+	
+	public LinkedList deleteKey(LinkedList list,int data) {
+		Node temp = firstNode;
+		Node parent = null;
+		if(temp != null && temp.data == data) {
+			firstNode = temp.nextAddress;
+			return list;
+		}
+		
+		while(temp != null && temp.data != data) {
+			parent = temp;
+			temp = temp.nextAddress; 
+		}
+		
+		if(temp != null) {
+			parent.nextAddress = temp.nextAddress;
+			System.out.println(data+" deleted");
+		}else {
+			System.out.println("Data not found");
+		}
+		
+		return list;
+		
+	}
+
+	
+	
+}
---
+++ datastructure/linkedlist/LinkedListMain.java
@@ -0,0 +1,66 @@
+package com.fellowship.datastructure.linkedlist;
+import java.util.*;
+
+public class LinkedListMain {
+	
+	public static void main(String args[]) {
+		
+		showOptionMenu();
+		
+	}
+	public static void showOptionMenu() {
+		LinkedList list = new LinkedList();
+		boolean exit =true;
+		do {
+			System.out.println("Make Your Choice..");
+			System.out.println("1.Add Data\n2.Delete data \n3.Display List \n4.Size of list\n5.Search data\n6.Add data at front.\n7.Exit");
+			Scanner scanner = new Scanner(System.in);
+			int choice = scanner.nextInt();
+					
+			switch(choice) {
+				case 1:
+					System.out.println("Enter data to add.");
+					int data = scanner.nextInt();
+					list.add(data);
+					break;
+				case 2:
+					System.out.println("Enter data to delete ");
+					int data1 = scanner.nextInt();
+					list.deleteKey(list, data1);
+					break;
+				case 3:
+					list.display();
+					break;
+				case 4:
+					int size = list.sizeOfList();
+					System.out.println("Size of list is "+size);
+					break;
+				case 6:
+					System.out.println("Enter data to at from front");
+					int data2 = scanner.nextInt();
+					list.addDataAtFront(data2);
+					break;
+				case 5:
+					System.out.println("Enter data to search");
+					int data3 = scanner.nextInt();
+					boolean found = list.search(data3);
+					if(found) {
+						System.out.println("data found.");
+					}else {
+						System.out.println(data3+" is not present in linkedList");
+					}
+					break;
+				case 7:
+					System.out.println("You are exiting.");
+					exit = false;
+					break;
+				default:
+					System.out.println("Enter choice Only from Menu.");
+			}
+			
+		}while(exit);
+		
+	}
+
+
+}
